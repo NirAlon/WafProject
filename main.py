e3d3db877f09157217decfbb844159376a7618b2
Nir Alon <niralon99@gmail.com>
2020-12-30 13:30:24 +0200

    Updating the main file, Add mitmproxy scanner package and Payloads

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from mitmproxyscanandread import scancoomandsfromfile
+import difflib
 
-print(scancoomandsfromfile.makeCommands())
+from mitmproxyscanandread import scancoomandsfromfile
+import csv
+
+
+list = (scancoomandsfromfile.makeCommands())
+print(list[2])
+with open('./Payloads/SQLi_Payloads.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile)
+    for row in spamreader:
+        similarity = difflib.SequenceMatcher(None, 'aaaaaaaa', row).ratio()
+        print(similarity*100)

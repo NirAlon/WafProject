a60848c89134ed63948fe21dce4c83d368d746b4
Nir Alon <niralon99@gmail.com>
2021-09-02 17:21:39 +0300

    django-rest-framework-api

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,5 @@
 from main.models import *
 from django.contrib import admin
-from myapi.models import Hero, Waf
 
 admin.site.register(Logger)
 admin.site.register(UserDemo)
-admin.site.register(Hero)
-admin.site.register(Waf)

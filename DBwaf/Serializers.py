32e8be05fd614067afe20e8e3fd39bbf451b2164
niralon <niralon99@gmail.com>
2025-05-18 17:02:35 -0600

    clean project

--- DBwaf/Serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from main.models import Logger
-
-
-class LoggerSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Logger
-		fields = ['email', 'date', 'threshold', 'type_attack', 'command', 'if_warn']
--- main/admin.py
+++
@@ -1,5 +0,0 @@
-from main.models import *
-from django.contrib import admin
-
-admin.site.register(Logger)
-admin.site.register(UserDemo)
